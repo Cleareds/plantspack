8a875bd3f5afc384127808936a23f4da08101aa9
Anton Kravchuk anton.kravchuk@vaimo.com <anton.kravchuk@vaimo.com>
2025-11-13 10:54:00 +0100

    Fix builds

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Exclude mobileapp folder from Next.js compilation
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/node_modules', '**/mobileapp/**'],
+    };
+    return config;
+  },
+
   async headers() {
     return [
       {
