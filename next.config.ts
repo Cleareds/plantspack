764d337b0f1220f2f4b2cda341406283ca67ad56
Anton Kravchuk anton.kravchuk@vaimo.com <anton.kravchuk@vaimo.com>
2026-01-04 14:31:57 +0100

    Add OAuth provider image domains to Next.js config
    - Add Google profile images (googleusercontent.com)
    - Add Facebook profile images (graph.facebook.com, fbsbx.com)
    - Fixes broken OAuth avatar images

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,22 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'graph.facebook.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
     ],
   },
 
